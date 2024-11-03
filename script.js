c185039519690eabc2b33a62a9ef2d37fc537bfa
petrebagashvili <petre.bagashvili204@ens.tsu.edu.ge>
2024-11-03 11:53:52 +0400

    homework

---
+++ script.js
@@ -0,0 +1,36 @@
+let x=15;
+x*=x;
+if (x>100) {
+    console.log(x,">100");
+} else if (x==100) {
+    console.log(x,"=100");
+} else {
+    console.log(x,"<100");
+    
+}
+
+
+let y=70;
+if (y>50 && y<100) {
+    console.log(y,"<100 and",y,">50");
+} else if (x==100) {
+    console.log(y,"=100");
+} else if (y==50) {
+    console.log(y,"=50");
+} else if (y>100) {
+    console.log(y,"<100");
+} else {
+    console.log(y,"<50");   
+}
+
+
+ let z=0;
+ if (z==0) {
+    console.log(z ,"=0");
+ } else if (z<0) {
+    console.log(z ,"<0");
+
+ } else {
+    
+    console.log(z ,">0");
+ }
