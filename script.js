854085d4c221b3386b73ed837642e3218d829550
petrebagashvili <petre.bagashvili204@ens.tsu.edu.ge>
2024-11-05 16:34:52 +0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ if (x>100) {
 let y=70;
 if (y>50 && y<100) {
     console.log(y,"<100 and",y,">50");
-} else if (x==100) {
+} else if (y==100) {
     console.log(y,"=100");
 } else if (y==50) {
     console.log(y,"=50");
